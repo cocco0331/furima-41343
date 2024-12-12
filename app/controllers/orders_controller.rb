9aa2481b16a661df6bd47f8b57e13a2e1314c142
cocco0331 <zzzcoccozzz@gmail.com>
2024-12-13 00:25:22 +0900

    商品出品機能再修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -39,6 +39,6 @@ class OrdersController < ApplicationController
 
   def set_index
     @item = Item.find(params[:item_id])
-    redirect_to root_path if current_user.id == @item.user_id && @item.present?
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 end
